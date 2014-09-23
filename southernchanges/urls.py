143e49ef4d08186fd096faaf48ac8e76735c09ce
Sara Palmer <sepalme@emory.edu>
2014-09-23 15:52:39 -0400

    update url pattern for article display

--- southernchanges/urls.py
+++ southernchanges/urls.py
@@ -1,11 +1,12 @@
+from django.conf.urls import patterns, include, url
 #from django.conf.urls import patterns, include, url
-from django.conf.urls.defaults import *
+#from django.conf.urls.defaults import *
 from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls import patterns
-from django.core.urlresolvers import reverse
-from django.contrib.sitemaps import Sitemap, FlatPageSitemap, GenericSitemap
+#from django.conf.urls.static import static
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from django.conf.urls import patterns
+#from django.core.urlresolvers import reverse
+#from django.contrib.sitemaps import Sitemap, FlatPageSitemap, GenericSitemap
 # from blog.models import Entry
 
 # Uncomment the next two lines to enable the admin:
@@ -39,7 +40,7 @@ urlpatterns = patterns('southernchanges_app.views',
     url(r'^topics$', 'topics', name='topics'),
     url(r'^(?P<doc_id>[^/]+)/contents$', 'issue_toc', name="issue_toc"),
     url(r'^(?P<doc_id>[^/]+)/issue$', 'issue_display', name="issue_display"),
-    url(r'^(?P<doc_id>[^/]+)/(?P<div_id>[^/]+)$', 'article_display', name='article_display'),
+    url(r'^(?P<doc_id>[^/]+)/(?P<div_id>[^/]+)/$', 'article_display', name='article_display'),
     url(r'^(?P<doc_id>[^/]+)/xml/$', 'issue_xml', name='issue_xml'),
     url(r'^(?P<topic_id>[^/]+)/articles/$', 'topic_toc', name='topic_toc'),
     # the sitemap
@@ -50,6 +51,3 @@ if settings.DEBUG:
   urlpatterns += patterns(
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT } ),
 )
-
-
-
